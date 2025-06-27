eb1bc96838ce4f944e766c245e59451eadd38b81
Will Dennis <williamldennis@gmail.com>
2025-06-27 09:54:15 -0400

    removed disabled input state

--- src/components/chunks/ChatArea.tsx
+++ src/components/chunks/ChatArea.tsx
@@ -4,6 +4,7 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useChat } from '@ai-sdk/react';
+import { useRef } from "react";
 
 type ChatAreaProps = {
     name: string | null,
@@ -11,7 +12,13 @@ type ChatAreaProps = {
 
 
 export default function ChatArea({ name }: ChatAreaProps) {
+    const inputRef = useRef<HTMLInputElement>(null)
     const { messages, input, handleInputChange, handleSubmit, status, stop } = useChat({});
+
+    const customSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        await handleSubmit(e)
+        inputRef.current?.focus()
+    }
 
     return (
         <div className="container flex flex-col items-center justify-center gap-6">
@@ -53,12 +60,13 @@ export default function ChatArea({ name }: ChatAreaProps) {
             </ScrollArea >
 
             <div className="flex w-full max-w-sm items-center gap-2">
-                <form onSubmit={handleSubmit} className="flex w-full">
+                <form onSubmit={customSubmit} className="flex w-full">
                     <Input className="mr-2"
+                        ref={inputRef}
                         name="prompt"
                         value={input}
                         onChange={handleInputChange}
-                        disabled={status !== 'ready'} />
+                    />
                     {
                         (status === 'submitted' || status === 'streaming') ? (
                             <Button
